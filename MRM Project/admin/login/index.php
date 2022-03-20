b39146bce5b599aa7118d7160fa9522aba30828f
phamquanghuy13112001 <phamquanghuy13112001@gmail.com>
2022-03-20 17:39:31 +0700

    Form đăng nhập

--- MRM Project/admin/login/index.php
+++ MRM Project/admin/login/index.php
@@ -22,7 +22,46 @@
 <body>
 
 	<div class="login-page bk-img" style="background-image: url(img/front-image.jpg);">
-		
+		<div class="form-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<h1 class="text-center text-bold text-light mt-4x">Đăng nhập tài khoản</h1>
+						<div class="well row pt-2x pb-3x bk-light">
+							<div class="col-md-8 col-md-offset-2">
+								<form method="post">
+
+									<label for="" class="text-uppercase text-sm">Tài Khoản </label>
+									<input type="text" placeholder="Username" name="username" class="form-control mb">
+
+									<label for="" class="text-uppercase text-sm">Mật Khẩu</label>
+									<input type="password" placeholder="Password" name="password" class="form-control mb">
+
+
+
+									<button class="btn btn-primary btn-block" name="login" type="submit">Đăng nhập</button>
+
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
 </html>
+
