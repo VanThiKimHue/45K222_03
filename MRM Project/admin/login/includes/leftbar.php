d81966947a407884508b96e0977b7826249befe9
Phạm Quốc Hưng <phamquochung0299@gmail.com>
2022-03-29 11:26:50 +0700

    lb

---
+++ MRM Project/admin/login/includes/leftbar.php
@@ -0,0 +1,28 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">DANH MỤC QUẢN LÝ</li>
+				<!-- <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li> -->
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Hãng xe</a>
+<ul>
+<li><a href="create-brand.php">Tạo hãng mới</a></li>
+<li><a href="manage-brands.php">Quản lý hãng xe</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Phương tiện</a>
+					<ul>
+						<li><a href="post-avehical.php">Thêm xe</a></li>
+						<li><a href="manage-vehicles.php">Quản lý xe</a></li>
+					</ul>
+				</li>
+				<li><a href="#"><i class="fa fa-files-o"></i> Người dùng</a>
+				<ul>
+				<li><a href="add-users.php">Thêm khách hàng</a></li>
+				<li><a href="reg-users.php">Danh sách</a></li>
+			    </ul>
+			    </li>
+
+			</ul>
+		</nav>
