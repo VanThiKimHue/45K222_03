324b88bff39871cd2adabc8d19c75fa174a78766
khanhlinhnguyenpham <97875248+khanhlinhnguyenpham@users.noreply.github.com>
2022-04-06 16:18:15 +0700

    logout

--- MRM Project/admin/login/includes/header.php
+++ MRM Project/admin/login/includes/header.php
@@ -7,7 +7,7 @@
 				<a href="#"><img src="img/Snapseed.jpg" class="ts-avatar hidden-side" alt=""> Tài khoản <i class="fa fa-angle-down hidden-side"></i></a>
 				<ul>
 					<li><a href="change-password.php">Đổi mật khẩu</a></li>
-					
+					<li><a href="logout.php">Đăng xuất</a></li>
 				</ul>
 			</li>
 		</ul>
