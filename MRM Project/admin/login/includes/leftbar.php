5483ab570fd4ed7f85c1d17727095308663d61a4
khanhlinhnguyenpham <97875248+khanhlinhnguyenpham@users.noreply.github.com>
2022-04-25 10:40:55 +0700

    capnhat leftbar.php

--- MRM Project/admin/login/includes/leftbar.php
+++ MRM Project/admin/login/includes/leftbar.php
@@ -4,20 +4,20 @@
 				<li class="ts-label">DANH MỤC QUẢN LÝ</li>
 				<!-- <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li> -->
 			
-<li><a href="#"><i class="fa fa-files-o"></i> Hãng xe</a>
+<li><a href="#"><i class="fa fa-motorcycle"></i> Hãng xe</a>
 <ul>
 <li><a href="create-brand.php">Tạo hãng mới</a></li>
 <li><a href="manage-brands.php">Quản lý hãng xe</a></li>
 </ul>
 </li>
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Phương tiện</a>
+<li><a href="#"><i class="fa fa-motorcycle"></i> Phương tiện</a>
 					<ul>
 						<li><a href="post-avehical.php">Thêm xe</a></li>
 						<li><a href="manage-vehicles.php">Quản lý xe</a></li>
 					</ul>
 				</li>
-				<li><a href="#"><i class="fa fa-files-o"></i> Khách hàng</a>
+				<li><a href="#"><i class="fa fa-users"></i> Khách hàng</a>
 				<ul>
 				<li><a href="add-users.php">Thêm khách hàng</a></li>
 				<li><a href="reg-users.php">Danh sách</a></li>
@@ -29,14 +29,20 @@
 			    	<li><a href="manage-bookings.php">Danh sách</a></li>
 			     </ul>
 			    </li>
-				<li><a href="#"><i class="fa fa-users"></i> Quản lý bảo dưỡng, sửa chữa, bảo hiểm</a>
+				<li><a href="#"><i class="fa fa-files-o"></i> Quản lý bảo dưỡng, bảo hiểm</a>
 			     <ul>
 			     	<li><a href="manage-maintenance.php">Quản lý bảo dưỡng</a></li>
-			     	<li><a href="manage-repair.php">Quản lý sửa chữa</a></li>
 			    	<li><a href="manage-insurance.php">Quản lý bảo hiểm</a></li>
+					<li><a href="manage-repair.php">Quản lý sữa chữa</a></li>
 			     </ul>
 			    </li>
-
+				<li><a href="#"><i class="fa fa-files-o"></i>Báo cáo</a>
+				<ul>
+				<li><a href="report-sales.php">Báo cáo doanh thu</a></li>
+				<li><a href="report-expenses.php">Báo cáo chi phí bảo hiểm</a></li>
+				<li><a href="report-repair.php">Báo cáo chi phí sữa chữa</a></li>
+			    </ul>
+			    </li>
 
 			</ul>
 		</nav>
